056574e1c09b4a5efdd1443893e2eff20bf44000
dong <dwx_job@163.com>
2023-06-25 21:57:22 +0800

    表定义

--- app/dao/stock_base_info.py
+++ app/dao/stock_base_info.py
@@ -4,8 +4,8 @@ from tortoise import Model, fields
 class StockBaseInfo(Model):
     id = fields.BigIntField(pk=True, source_field='id', generated=True,description='id')
     name = fields.CharField(100, source_field='name',description='名称')
-    code = fields.CharField(50, unique=True, source_field='code')
-    latestPrice = fields.FloatField(source_field='latest_price', null=True)
+    code = fields.CharField(50, unique=True, source_field='code',description='代码')
+    latestPrice = fields.FloatField(source_field='latest_price', null=True,description='最新价格')
 
     def __str__(self):
         return f"I am {self.name}"
