86977f3d23e907fc948fc3cac9e19a9256385d19
dong <dwx_job@163.com>
2023-07-08 21:51:55 +0800

    kline

--- app/dao/user.py
+++ app/dao/user.py
@@ -3,7 +3,9 @@ from tortoise import Model, fields
 
 class User(Model):
     id = fields.IntField(pk=True, source_field='id', generated=True)
-    name = fields.CharField(50)
+    name = fields.CharField(50, source_field='name', unique=True)
+    sex = fields.BooleanField(source_field='sex')
+    password = fields.CharField(50, source_field='password')
 
     def __str__(self):
         return f"I am {self.name}"
